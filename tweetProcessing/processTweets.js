18c3c122f529e0eb818aa9a8d9bddf87e37a98cd
rishuverma <rishuverma1997@gmail.com>
2021-11-07 21:40:21 +0530

    added spam filter

--- tweetProcessing/processTweets.js
+++ tweetProcessing/processTweets.js
@@ -5,32 +5,24 @@ const { readQueryObj } = require("./readQueryObjFromFile");
 const { readSinceId } = require("./readSinceId");
 const { writeSinceId } = require("./writeSinceId");
 const { writeQueryObj } = require("./writeQueryObj");
+const { spamFilter } = require("./spamFilter");
 const processIncomingTweets = async () => {
   try {
     // const since_idObj = await readSinceId();
     const queryObj = await readQueryObj();
-    // const clonequeryObj = { ...queryObj };
-    // console.log("query object here", queryObj);
     for (let eachQuery of queryObj.queries) {
-      // console.log("query right now", eachQuery);
       const tweets = await getTweets(eachQuery);
-      // tweets=[...tweets,newTweets.statuses];
-
-      // const tweets = await getTweets();
-      // const tweetIdArray = [];
-      // console.log("tweets received ", tweets);
       for (let eachTweet of tweets.statuses) {
         try {
-          // tweetIdArray.push(eachTweet.id_str);
           if (
             eachTweet.id_str != eachQuery.since_id &&
-            !eachTweet.retweeted_status
+            // !eachTweet.retweeted_status
+            spamFilter(eachTweet)
           ) {
             await postRetweets(eachTweet.id_str);
             console.log("tweeted successfuly", eachTweet.id_str);
             eachQuery.since_id = eachTweet.id_str;
           }
-          // console.log("after update query obj", queryObj);
         } catch (err) {
           console.log("unsuccessful reweet", eachTweet.id_str);
           console.log(err);
