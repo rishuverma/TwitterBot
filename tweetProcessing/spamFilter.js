d34f8cfc92cec58d3ade9b028d647d6fbacdf465
rishuverma <rishuverma1997@gmail.com>
2021-11-08 10:21:15 +0530

    minor

--- tweetProcessing/spamFilter.js
+++ tweetProcessing/spamFilter.js
@@ -1,8 +1,8 @@
 const spamFilter = async (tweet) => {
   try {
-    if (tweet.retweeted_status) return false;
-    if (tweet.text.trim().split(" ").length < 5) return false;
-    if (tweet.entities.hashtags.length > 4) return false;
+    if (await tweet.retweeted_status) return false;
+    if ((await tweet.text.trim().split(" ").length) < 5) return false;
+    if ((await tweet.entities.hashtags.length) > 4) return false;
     if (!/d(?:ays?)?\W*(\d+)/gi.test(tweet.text)) return false;
     return true;
   } catch (err) {
